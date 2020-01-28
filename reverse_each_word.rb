fb62dabf6e452cea66396fef5daf6268d11af099
Tammy Lam <lamtammy1515@gmail.com>
2020-01-28 03:12:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,6 @@
 def reverse_each_word(sentence1)
-#  sentence1 = ["Hello there, and how are you?"]
-#  sentence1.each do |phrase|
-  #return "#{phrase.reverse}"
-  sentence1 = str.split /\b/
-  new_arr = sentence1.collect {|a| a.reverse}
-  new_arr.join
+  sentence1 = ["Hello there, and how are you?"]
+  sentence1.each do |phrase|
+  return "#{phrase.reverse}"
+ 
 end 
