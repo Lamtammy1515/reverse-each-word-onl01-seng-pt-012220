2fb0a421019f35f7c5a826daa7100dd3836f13fb
Tammy Lam <lamtammy1515@gmail.com>
2020-01-28 03:05:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
 def reverse_each_word(sentence1)
   sentence1 = []
-  reverse_each_word.each {|phrase| sentence1 << phrase.reverse}
+  return reverse_each_word.each {|phrase| sentence1 << phrase.reverse}
 end 
