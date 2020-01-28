025d7cd7b8a42fb6c9faaf24571be781f079527a
Tammy Lam <lamtammy1515@gmail.com>
2020-01-28 02:50:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,6 @@
 def reverse_each_word(sentence1)
-  sentence1 = "Hello there, and how are you?"
- return sentence1.reverse_inplace!
+  sentence1 = ["Hello there, and how are you?"]
+  sentence1.each do |phrase|
+  return phrase.reverse_inplace!
+end 
 end 
