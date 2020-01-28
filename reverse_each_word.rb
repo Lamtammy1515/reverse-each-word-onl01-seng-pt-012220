cf75741c0d914c4d4e10f53e9fe5d2b11e1503a3
Tammy Lam <lamtammy1515@gmail.com>
2020-01-28 03:20:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence1)
  phrase = sentence1.split(" ")
  reversed = []
   phrase.collect do|sentence|
-      my_array << sentence.reverse
+      reversed << sentence.reverse
     end 
     return reversed.join(" ")
 end 
